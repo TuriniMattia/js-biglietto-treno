7140388df2319d8e4b2ae39d92091895a2f6f8f6
TuriniMattia <turinimattia96@gmail.com>
2024-03-05 10:25:27 +0100

    [add]correzzione

--- app.js
+++ app.js
@@ -6,15 +6,23 @@
 // L’output del prezzo finale va stampato in console in forma “umana” cioè con al massimo due decimali, per indicare centesimi sul prezzo. Questo richiederà un minimo di ricerca.
 
 // 1-verificare eta del passeggero
-    let customersAge = prompt("Inserisci l'età") //string
+    let customersAge = parseFloat(prompt("Inserisci l'età"))//string
+    console.log("età", customersAge)
 // 2-verificare numero di kilometri
-    let customersKm = prompt("Inserisci km tratta") //string
+    let customersKm = parseFloat(prompt("Inserisci km tratta"))
+    console.log("tratta", customersKm) //string
 // 3-calcolare prezzo del biglietto in base ai km (0,21 euro/km)
-    let ticketCost = (customersKm * 0,21)
-    ticketCost.toFixed(2);
-    console.log("Total:", ticketCost)
+    let ticketCost = customersKm * 0.21
+    console.log("Totale:", ticketCost)
+    ticketCost
     
 // 4-SE (IF) età passeggero < 18 === sconto 20%
+    if(customersAge < 18){
+        sconto = ticketCost - (ticketCost * 0.2)
+    }
     
 // 5-ALTRIMENTI SE (EVEN IF) età  del passeggero > 65 === sconto 40%
+    else if(customersAge > 65){
+        sconto = ticketCost - (ticketCost * 0,4) 
+    }
 // 6-ALtrimenti età passeggero età passeggero >= 18 e <65 prezzo pieno
